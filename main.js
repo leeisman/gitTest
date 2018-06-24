4b558db488eb28e67a0ec5fb764902edee1bd84c
Frankie <101m06008@stud.sju.edu.tw>
2018-06-24 10:51:42 +0800

    first

---
+++ main.js
@@ -0,0 +1,75 @@
+alert(1)
+alert(3)
+
+
+function test() {
+    alert('test1')
+}
+
+function pp(e) {
+    alert(1);
+}
+
+function pp() {
+
+}
+
+function pp() {
+
+}
+
+function pp(p) {
+
+}
+
+function pp() {
+
+}
+
+function pp(e) {
+
+}
+
+function pp() {
+
+}
+
+function show() {
+    alert('show1')
+}
+
+function newTest() {
+    //
+}
+
+function newCheck() {
+    //
+}
+
+function new1() {
+    alert(1);
+}
+
+function new1() {
+    alert('rebase');
+}
+
+function new2() {
+    alert('rebase')
+}
+
+function one() {
+
+}
+
+function two() {
+
+}
+
+function three() {
+
+}
+
+function newFeature1111() {
+    alert('newFeature1111')
+}
